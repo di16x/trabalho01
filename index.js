52ad2adf375a69057ed83d7e40cb51bbcad04ee5
Diego Felix Tomé <diego_tome16@live.com>
2024-08-21 00:07:16 -0300

    requisição do login

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const host = '0.0.0.0';
 const porta = 3000;
 const app = express();
 
-app.use(express.static('./publico'))
+app.use(express.static('./publico'));
+app.use(express.static('./privado'));
+
+app.get('/login',(requisicao,resposta) => {resposta.redirect('/login.html');
+    
+});
+
 
 app.listen(porta,host, () => {
     console.log(`Servidor rodando em http://${host}:${porta}`);
