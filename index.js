9a704b153d58d99a595298e6a1369aba3e96b6ee
Diego Felix Tomé <diego_tome16@live.com>
2024-08-21 09:25:35 -0300

    atualização css

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const host = '0.0.0.0';
 const porta = 3000;
 const app = express();
 
+
+app.use(express.urlencoded ({extended:true}));
+
 app.use(express.static('./publico'));
 app.use(express.static('./privado'));
 
--- seguranca/autenticar.js
+++ seguranca/autenticar.js
@@ -5,8 +5,16 @@ export default function autenticar(requisicao,resposta){
     resposta.redirect('/menu.html');
   }
   else {
+    resposta.write('<html>');
+    resposta.write('<head>');
+    resposta.write('<title> Falha </title>');
+    resposta.write('<meta charset = "utf-8">')
+    resposta.write('</head>');
+    resposta.write('<body>');
     resposta.write('<p>Usuário ou senha inválidos</p>');
     resposta.write('<a href = "/login.html">Voltar</a>');
+    resposta.write('</body>');
+    resposta.write('</html>');
     resposta.end();
   }
 }
